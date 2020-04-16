e6a8f09329db1f3a13de10ef66df5981bf3cb7db
tanviGanesh <tanvisalkar@yahoo.in>
2020-04-16 20:13:49 +0530

    all generic models done

--- crm-plugin/controllers/common/base.js
+++ crm-plugin/controllers/common/base.js
@@ -5,37 +5,107 @@
  *
  * @description: A set of functions called "actions" of the `crm-plugin` plugin.
  */
+const { sanitizeEntity } = require("strapi-utils");
+
+function getTable(url) {
+  let urlArr = url.split("/");
+  console.log("ctx", urlArr);
+  let table = "";
+  if (urlArr.length >= 3) table = urlArr[2];
+  table = table == "countries" ? "country" : table.slice(0, table.length - 1);
+  return table;
+}
+
 function Base() {
   /**
    * Default action.
    *
    * @return {Object}
    */
-  this.getTable = (url) => {
-    let urlArr = url.split("/");
-    console.log("ctx", urlArr);
-    let table = "";
-    if (urlArr.length >= 3) table = urlArr[2];
-    return table;
-  };
 
   this.find = async (ctx) => {
-    console.log("ctx", ctx);
-    let table = this.getTable(ctx.originalUrl);
-    console.log("table", table.splice(table.length));
+    let table = getTable(ctx.originalUrl);
     try {
-      let state;
+      let entity;
       if (ctx.query._q) {
-        state = await strapi.query(table, "crm-plugin").search(ctx.query);
+        entity = await strapi.query(table, "crm-plugin").search(ctx.query);
       } else {
-        state = await strapi.query(table, "crm-plugin").find(ctx.query);
+        entity = await strapi.query(table, "crm-plugin").find(ctx.query);
       }
 
-      return state.map((entity) =>
-        sanitizeEntity(entity, {
+      return entity.map((ent) =>
+        sanitizeEntity(ent, {
           model: strapi.plugins["crm-plugin"].models[table],
         })
       );
+    } catch (error) {
+      console.error(error);
+      return { error: error.message };
+    }
+  };
+
+  this.findOne = async (ctx) => {
+    const { id } = ctx.params;
+    let table = getTable(ctx.originalUrl);
+    try {
+      const entity = await strapi.query(table, "crm-plugin").findOne({ id });
+      return sanitizeEntity(entity, {
+        model: strapi.plugins["crm-plugin"].models[table],
+      });
+    } catch (error) {
+      console.error(error);
+      return { error: error.message };
+    }
+  };
+
+  this.count = async (ctx) => {
+    const { id } = ctx.params;
+    let table = getTable(ctx.originalUrl);
+    try {
+      if (ctx.query._q) {
+        return strapi.query(table, "crm-plugin").countSearch(ctx.query);
+      }
+      return strapi.query(table, "crm-plugin").count(ctx.query);
+    } catch (error) {
+      console.error(error);
+      return { error: error.message };
+    }
+  };
+
+  this.create = async (ctx) => {
+    let entity;
+    let table = getTable(ctx.originalUrl);
+    try {
+      if (ctx.params.id) {
+        const { id } = ctx.params;
+        entity = await strapi
+          .query(table, "crm-plugin")
+          .update({ id }, ctx.request.body);
+        return sanitizeEntity(entity, {
+          model: strapi.plugins["crm-plugin"].models[table],
+        });
+      } else {
+        entity = await strapi
+          .query(table, "crm-plugin")
+          .create(ctx.request.body);
+        return sanitizeEntity(entity, {
+          model: strapi.plugins["crm-plugin"].models[table],
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      return { error: error.message };
+    }
+  };
+
+  this.delete = async (ctx) => {
+    const { id } = ctx.params;
+    let table = getTable(ctx.originalUrl);
+    try {
+      const entity = await strapi.query(table, "crm-plugin").delete({ id });
+      return sanitizeEntity(entity, {
+        model: strapi.plugins["crm-plugin"].models[table],
+      });
     } catch (error) {
       console.error(error);
       return { error: error.message };
