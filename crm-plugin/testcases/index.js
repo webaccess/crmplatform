60a2295301b6dc6ad8be72eb9ed276c7739718f8
vishaltambe70 <vishal.tambe70@gmail.com>
2020-04-24 14:38:51 +0530

    merging master

--- crm-plugin/testcases/index.js
+++ crm-plugin/testcases/index.js
@@ -3,12 +3,15 @@
 
 var State = require("./state.test.js");
 var Contact = require("./contact.test.js");
+var Activity = require("./activity.test.js");
 
 var state = new State();
 var contact = new Contact();
+var activity = new Actibvity();
 module.exports = {
   index: async () => {
     var s = await state.index();
     var c = await contact.index();
+    var act = await activity.index();
   },
 };
