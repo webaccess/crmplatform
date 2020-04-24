b9180efdacbf437b73b6641d5fb1263115febde8
tanviGanesh <tanvisalkar@yahoo.in>
2020-04-24 19:56:58 +0530

    policy added

--- crm-plugin/testcases/state.test.js
+++ crm-plugin/testcases/state.test.js
@@ -12,6 +12,9 @@ function State() {
     var find = await this.emptyTestcase("find");
     var create = await this.emptyTestcase("create");
     var deleteMethod = await this.emptyTestcase("delete");
+    var finds = await this.reqParamsTestcase("find");
+    var creates = await this.reqParamsTestcase("create");
+    var deleteMethods = await this.reqParamsTestcase("delete");
   };
 
   /* this method calls all testcases for empty params check */
@@ -46,6 +49,51 @@ function State() {
             return { error: message };
           },
         };
+        break;
+    }
+    return await testcase.test("state", method, methodParams);
+  };
+
+  /* this method calls all testcases for required params check */
+  this.reqParamsTestcase = async (method) => {
+    let methodParams = {};
+    //this switch case needs to handle all methods of district controller
+    switch (method) {
+      case "find":
+        methodParams = {
+          originalUrl: "/crm-plugin/states",
+          query: {},
+          badRequest: (error, message) => {
+            return { error: message };
+          },
+        };
+        console.log("Required params test not applicable for find method");
+        console.log("-------------");
+        return;
+        break;
+      case "create":
+        methodParams = {
+          originalUrl: "/crm-plugin/states",
+          request: {
+            body: { is_active: false },
+          },
+          params: {},
+          badRequest: (error, message) => {
+            return { error: message };
+          },
+        };
+        break;
+      case "delete":
+        methodParams = {
+          originalUrl: "/crm-plugin/states",
+          params: {},
+          badRequest: (error, message) => {
+            return { error: message };
+          },
+        };
+        console.log("Required params test not applicable for delete method");
+        console.log("-------------");
+        return;
         break;
     }
     return await testcase.test("state", method, methodParams);
