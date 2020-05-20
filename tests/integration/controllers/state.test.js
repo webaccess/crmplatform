08c5803285e1dc683bd1a228eb78610e26a95c78
Ankitashinde0 <ankitashinde0807@gmail.com>
2020-05-20 14:34:29 +0530

    Statements changed in testcases

--- tests/integration/controllers/state.test.js
+++ tests/integration/controllers/state.test.js
@@ -22,7 +22,7 @@ describe("States Module Endpoint", function () {
   describe("Find Method", function () {
     // case for empty params done here
     describe("GET /crm-plugin/states", function () {
-      it("Empty params test case", function (done) {
+      it("responds with all records when empty params test case is executed", function (done) {
         request(SERVER_URL)
           .get("/crm-plugin/states")
           .set("Authorization", "Bearer " + JWT)
@@ -39,7 +39,7 @@ describe("States Module Endpoint", function () {
   describe("Create Method", function () {
     // case for empty,required and correct params for Create method done here
     describe("POST /crm-plugin/states/", function () {
-      it("Empty params test case", function (done) {
+      it("should not create an entry when empty params test case is executed", function (done) {
         request(SERVER_URL)
           .post("/crm-plugin/states")
           .send({})
@@ -52,7 +52,7 @@ describe("States Module Endpoint", function () {
           });
       });
 
-      it("Required params test case", function (done) {
+      it("should not create an entry when required params test case is executed", function (done) {
         request(SERVER_URL)
           .post("/crm-plugin/states")
           .send({
@@ -67,7 +67,7 @@ describe("States Module Endpoint", function () {
           });
       });
 
-      it("Correct params test case", function (done) {
+      it("should create an entry when correct params test case is executed", function (done) {
         request(SERVER_URL)
           .post("/crm-plugin/states")
           .send({
@@ -88,7 +88,7 @@ describe("States Module Endpoint", function () {
   describe("Update Method", function () {
     // case for correct params done for update method
     describe("PUT /crm-plugin/states/:id", function () {
-      it("Updating params test case", function (done) {
+      it("should update the data when correct params test case is executed", function (done) {
         request(SERVER_URL)
           .put("/crm-plugin/states/" + dataId)
           .send({
@@ -106,9 +106,9 @@ describe("States Module Endpoint", function () {
   });
 
   describe("FindOne Method", function () {
-    // case for empty params done here
+    // case for correct params done here
     describe("GET /crm-plugin/states/:id", function () {
-      it("Empty params test case", function (done) {
+      it("responds with matching records when correct params test case is executed", function (done) {
         request(SERVER_URL)
           .get("/crm-plugin/states")
           .send({
@@ -128,7 +128,7 @@ describe("States Module Endpoint", function () {
   describe("Count Method", function () {
     // case for count done here
     describe("GET /crm-plugin/states/count", function () {
-      it("Empty params test case", function (done) {
+      it("should return data count when correct params test case is executed", function (done) {
         request(SERVER_URL)
           .get("/crm-plugin/states/count")
           .set("Authorization", "Bearer " + JWT)
@@ -145,7 +145,7 @@ describe("States Module Endpoint", function () {
   describe("Delete Method", function () {
     // case for correct params done here
     describe("DELETE /crm-plugin/states/:id", function () {
-      it("Correct params test case", function (done) {
+      it("should delete entry when correct params test case is executed", function (done) {
         request(SERVER_URL)
           .delete("/crm-plugin/states/" + dataId)
           .set("Authorization", "Bearer " + JWT)
