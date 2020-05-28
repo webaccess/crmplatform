85dfbd1408627ec1fbf8d3459e06a82ec6858605
suyesh <suyeshtiwari123@gmail.com>
2020-05-28 21:46:43 +0530

    made working directory changes

--- tests/integration/strapi.js
+++ tests/integration/strapi.js
@@ -5,15 +5,11 @@ let instance;
 
 async function setupStrapi() {
   if (!instance) {
-    console.log("process.cwd()",process.cwd());
-    let Dir = process.cwd();
-    let directory = Dir.splice
-    console.log("directory", directory);
-    /** the follwing code in copied from `./node_modules/strapi/lib/Strapi.js` */
-    await Strapi({dir:"../../"}).load()
+    let directory = process.cwd();
+    let StrapiDir = directory.split("/").slice(0,-2).join("/")
+    await Strapi({dir:StrapiDir}).load()
     .then(async function(response) {
       instance = strapi; // strapi is global now
-      // console.log("Instance of Strapi -----",instance)
       await instance.app
         .use(instance.router.routes()) // populate KOA routes
         .use(instance.router.allowedMethods()); // populate KOA methods
