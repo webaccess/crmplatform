0dd69d5e048ff6aee2a88d4ef66a6b0c336c32c4
pooja chaudhari <pooja.chaudhari@webaccessglobal.com>
2020-06-09 12:25:03 +0530

    Auto population script for inserting country,state & district data #83

--- services/utils.js
+++ services/utils.js
@@ -33,6 +33,13 @@ function checkParams(values, requiredValues) {
   return result;
 }
 
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
 module.exports = {
   checkParams,
+  asyncForEach,
 };
