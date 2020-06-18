ebe3a5e1059b3af928e927c7a0ff1b8f11c0c796
Ankitashinde0 <ankitashinde0807@gmail.com>
2020-06-18 17:43:17 +0530

    test

--- tests/integration/config/JWT.js
+++ tests/integration/config/JWT.js
@@ -1,4 +1,1 @@
-const JWT =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNTkyNDYyMDYxLCJleHAiOjE1OTUwNTQwNjF9.C5de4gagzc8k1MGo4jXNQ894rNwsxthWGeFfcWq5G0I";
-module.exports = { JWT };
-console.log("JWT in jwt.js", JWT);
+const JWT="undefined"; module.exports = { JWT,};
