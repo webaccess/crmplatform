57cf5d41a527d68cd2492c34bd9a49b685210430
Ankitashinde0 <ankitashinde0807@gmail.com>
2020-06-18 16:34:19 +0530

    Test

---
+++ tests/integration/config/JWT.js
@@ -0,0 +1,4 @@
+const JWT =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNTkyNDYyMDYxLCJleHAiOjE1OTUwNTQwNjF9.C5de4gagzc8k1MGo4jXNQ894rNwsxthWGeFfcWq5G0I";
+module.exports = { JWT };
+console.log("JWT in jwt.js", JWT);
